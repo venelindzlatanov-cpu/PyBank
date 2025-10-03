2f816907bc7edb059847f52749a72df38369dd67
vzlatanov <venelin.d.zlatanov@gmail.com>
2025-10-03 14:58:14 +0300

    add balance endpoint

--- ledger/views.py
+++ ledger/views.py
@@ -1,5 +1,6 @@
 # package: ledger.views
 from django.db import transaction as db_tx
+from django.db.models import Sum
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
@@ -16,6 +17,15 @@ from .serializers import (
 class AccountViewSet(viewsets.ModelViewSet):
     queryset = Account.objects.all().order_by("code")
     serializer_class = AccountSerializer
+    lookup_field = "code"
+
+    @action(detail=True, methods=["get"])
+    def balance(self, request, *args, **kwargs):
+        account = self.get_object()
+        total = (Posting.objects
+                 .filter(account=account)
+                 .aggregate(total=Sum("amount"))["total"] or 0)
+        return Response(data={"code": account.code, "balance": total}, status=200)
 
 
 class TransactionViewSet(viewsets.ReadOnlyModelViewSet):
